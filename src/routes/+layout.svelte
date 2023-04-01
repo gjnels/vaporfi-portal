d0652058064b06cb70804710e7b87d0b90c6e2eb
garrett <gnelson@duck.com>
2023-04-01 14:03:46 -0400

    Restyle and create basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Logo } from '../lib/components'
   import '../tailwind.css'
 </script>
 
@@ -6,4 +7,16 @@
   <title>VaporFi Columbus</title>
 </svelte:head>
 
-<slot />
+<div class="flex h-full flex-col">
+  <header class="flex h-12 items-center border-b border-slate-600 px-4 md:h-16">
+    <Logo />
+  </header>
+
+  <main class="mx-auto w-full max-w-screen-2xl grow p-4">
+    <slot />
+  </main>
+
+  <footer class="border-t border-slate-600 p-2 text-center text-xs font-light">
+    Created by Garrett Nelson &copy;2023
+  </footer>
+</div>
