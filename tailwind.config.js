5697a8d8d9417511ea04b06a1df905aaba134d09
garrett <gnelson@duck.com>
2023-04-01 12:57:12 -0400

    Update dependency versions, add tailwindcss, and modify prettierrc

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{svelte,html}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/forms')]
+}
