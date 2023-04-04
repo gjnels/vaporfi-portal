92cd54b00ccd07e67a906d6f50a58802bdc0c181
garrett <gnelson@duck.com>
2023-04-04 13:02:10 -0400

    Create checkbox component

---
+++ src/lib/components/forms/Checkbox.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { twMerge } from 'tailwind-merge'
+  import type { Color } from '.'
+  import Errors from './Errors.svelte'
+
+  export let label: string
+  export let name: string
+  export let checked: boolean
+  export let errors: string | string[] | undefined = undefined
+
+  export let color: Color = 'green'
+  export let containerStyles = ''
+  export let labelStyles = ''
+  export let checkboxStyles = ''
+</script>
+
+<label
+  class={twMerge(
+    'group flex w-fit cursor-pointer items-center gap-1.5',
+    containerStyles
+  )}
+>
+  <input
+    type="checkbox"
+    {name}
+    bind:checked
+    class={twMerge(
+      'peer cursor-pointer rounded-sm text-zinc-400 transition focus:ring-current focus:ring-offset-0  focus:checked:ring-white',
+      color === 'green' && 'text-green-500',
+      color === 'purple' && 'text-violet-500',
+      checkboxStyles
+    )}
+    {...$$restProps}
+  />
+
+  <span
+    class={twMerge(
+      'select-none transition group-focus-within:text-gray-400 group-hover:text-gray-400 peer-checked:text-white',
+      color === 'green' &&
+        'group-focus-within:text-green-400 group-hover:text-green-400',
+      color === 'purple' &&
+        'group-focus-within:text-violet-400 group-hover:text-violet-400',
+      labelStyles
+    )}>{label}</span
+  >
+</label>
+
+<Errors {errors} />
--- src/lib/components/forms/index.ts
+++ src/lib/components/forms/index.ts
@@ -2,5 +2,6 @@ export { default as Form } from './Form.svelte'
 export { default as Input } from './Input.svelte'
 export { default as Select } from './Select.svelte'
 export { default as RadioGroup } from './RadioGroup.svelte'
+export { default as Checkbox } from './Checkbox.svelte'
 
 export type Color = 'green' | 'purple' | undefined
