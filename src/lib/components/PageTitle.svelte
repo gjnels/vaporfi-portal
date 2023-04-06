d9aa63834271a0da0f0c9e422d236d28be22c5e8
garrett <gnelson@duck.com>
2023-04-06 15:21:11 -0400

    Change color of page title and remove optional slot

--- src/lib/components/PageTitle.svelte
+++ src/lib/components/PageTitle.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
   import { twMerge } from 'tailwind-merge'
 
-  export let title = ''
+  export let title: string
   export let styles = ''
 </script>
 
-<h1 class={twMerge('text-4xl font-semibold text-zinc-200', styles)}>
-  {#if $$slots.title}
-    <slot name="title" />
-  {:else}
-    {title}
-  {/if}
+<h1 class={twMerge('text-4xl font-semibold text-zinc-100', styles)}>
+  {title}
 </h1>
