7766423e000f3be1a40eada0ff24e7e77387c4b7
Garrett Nelson <gjnels@proton.me>
2023-05-05 16:34:29 -0400

    Only redirect when session is null, otherwise throw 403 error for unauthorized in admin layout load

--- src/routes/(admin)/+layout.server.ts
+++ src/routes/(admin)/+layout.server.ts
@@ -1,10 +1,6 @@
 import { error, redirect } from '@sveltejs/kit'
 
-export const load = async ({
-  locals: { getSession, supabase },
-  url,
-  depends
-}) => {
+export const load = async ({ locals: { getSession, supabase }, url, depends }) => {
   depends('supabase:auth')
 
   const session = await getSession()
@@ -19,12 +15,7 @@ export const load = async ({
     .eq('id', session.user.id)
     .single()
 
-  if (!profile) {
-    await supabase.auth.signOut()
-    throw redirect(307, `/login?redirectTo=${url.pathname}${url.search}`)
-  }
-
-  if (profile.role !== 'Admin') {
+  if (profile?.role !== 'Admin') {
     throw error(403) // Unauthorized
   }
 
