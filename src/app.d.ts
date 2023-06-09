25be4707e985f575dd9b675d44d44e8a2f02464b
Garrett Nelson <gjnels@proton.me>
2023-06-09 13:54:11 -0400

    Consolidate global type declaration to app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,10 @@ import type { Session, SupabaseClient } from '@supabase/supabase-js'
 import type { Database } from '$lib/types/supabase.types'
 
 declare global {
+  type Message =
+    | { type: 'success'; message: string | string[] }
+    | { type: 'error'; message: string | string[] }
+
   namespace App {
     // interface Error {}
     interface Locals {
